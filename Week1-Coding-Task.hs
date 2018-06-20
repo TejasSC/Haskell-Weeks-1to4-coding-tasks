62a3d49cc10484cf0cf320c149e7632e11a70b1a
Tejas Chandrasekar <tsctejas@gmail.com>
2018-06-20 14:48:15 +0800

    Implemented changes requested by Dmitri

--- Week1-Coding-Task.hs
+++ Week1-Coding-Task.hs
@@ -4,73 +4,70 @@
 --Calculate result mod 10: if 0, valid 
 
 ------------------------------------------------------------------------------
---Exercise 1: toDigits and toDigitsRev 
+-- Exercise 1: toDigits and toDigitsRev 
 ------------------------------------------------------------------------------
-toDigits :: Integer -> [Integer]
-toDigitsRev :: Integer -> [Integer]
-
---A list with elements reversed, so can use it later for reversing toDigits 
+-- A list with elements reversed, so can use it later for reversing toDigits 
 rev :: [Integer] -> [Integer]
-rev [] = []--base case 
-rev (s:l) = rev l ++ [s]--step case
+rev [] = []  -- base case 
+rev (s:l) = rev l ++ [s]  -- step case
 
+toDigits :: Integer -> [Integer]
 toDigits s 
-    | s <= 0 = []
-    | s < 10 = [s] -- They have only one digit, so one element list 
-    | otherwise = toDigits (s`div`10) ++ [s`mod`10]--mod10 results only give one digit  
+    | s <= 0 = []  
+    | otherwise = toDigits (s `div` 10) ++ [s `mod` 10]
 
-toDigitsRev s = rev (toDigits s)--using rev here to raise abstraction
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev s = rev (toDigits s)  -- using rev here to raise abstraction
 
 ------------------------------------------------------------------------------
---Exercise 2: doubleEveryOther
+-- Exercise 2: doubleEveryOther
 ------------------------------------------------------------------------------
---Length of a list 
+-- Length of a list 
 len :: [Integer] -> Integer
-len [] = 0--base case 
-len (_:l) = 1 + len l--step case
+len [] = 0  -- base case 
+len (_:l) = 1 + len l  -- step case
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] = [] --Base case, empty list unaffected 
-doubleEveryOther (s:[]) = [s]--Base case, single element lists also unaffected 
-doubleEveryOther (t:(s:l)) 
-    | (len l)`mod`2 == 0 = [2*t, s] ++ doubleEveryOther l 
-    | otherwise          = [t, 2*s] ++ doubleEveryOther l
+doubleEveryOther [] = []  -- Base case, empty list unaffected 
+doubleEveryOther (s:[]) = [s]  -- Base case, single element lists also unaffected 
+doubleEveryOther (t:(s:l)) = rev ([2*t, s] ++ doubleEveryOther (rev l))
 
 ------------------------------------------------------------------------------
---Exercise 3: sumDigits 
+-- Exercise 3: sumDigits 
 ------------------------------------------------------------------------------
---Uses toDigits function created before, with extra sum function 
---Sum of a list  
+-- Uses toDigits function created before, with extra sum function 
+-- Sum of a list  
 total :: [Integer] -> Integer
-total [] = 0--base case
-total (s:l) = s + total l--step case
+total [] = 0  -- base case
+total (s:l) = s + total l  -- step case
+
 sumDigits :: [Integer] -> Integer
-sumDigits [] = 0--base case 
-sumDigits (s:l) = total(toDigits s) + sumDigits l  
+sumDigits [] = 0  -- base case 
+sumDigits (s:l) = total (toDigits s) + sumDigits l  -- step case 
 
 ------------------------------------------------------------------------------
---Exercise 4: validate  
+-- Exercise 4: validate  
 ------------------------------------------------------------------------------
-validate :: Integer -> Bool 
 result :: Integer -> Integer
-result n = sumDigits(doubleEveryOther(toDigits n))--to raise abstraction 
-validate n 
-    | (result n)`mod`10 == 0 = True 
-    | otherwise = False 
+result n = sumDigits (doubleEveryOther (toDigits n))  -- to raise abstraction
+
+validate :: Integer -> Bool 
+validate n = result n `mod` 10 == 0
 
 ------------------------------------------------------------------------------
---Exercise 5: Towers of Hanoi 
+-- Exercise 5: Towers of Hanoi 
 ------------------------------------------------------------------------------
-type Peg = String--type synonym, i.e. Peg declared as synonym for String  
+type Peg = String  -- type synonym, i.e. Peg declared as synonym for String  
 type Move = (Peg, Peg)
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 a = "left"
 b = "centre"
 c = "right"
 hanoi n a b c 
-    | n <= 0 = []--base case, should handle all non positive inputs 
+    | n <= 0 = []  -- base case, should handle all non positive inputs 
     | otherwise = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a 
-    --step case recursively calls hanoi a step down
-    --note for all n ≥ 1, the move (a,b) is included exactly in the middle 
+    -- step case recursively calls hanoi a step down
+    -- note for all n ≥ 1, the move (a,b) is included exactly in the middle 
 
 
