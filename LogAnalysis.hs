3888071cb3f3630435cd481dbd286a7f4358e359
Tejas Chandrasekar <tsctejas@gmail.com>
2018-06-25 15:44:12 +0800

    Implemented requested changes for exercises in Week 2

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
 
@@ -14,15 +13,16 @@ import Log
 parseMessage :: String -> LogMessage  -- parsing function 
 
 -- $ means that anything after it will take precedence over anything before it 
-parseMessage s = case words s!!0 of
-    "I" -> LogMessage Info (read $ words s!!1 :: TimeStamp) (unwords $ drop 2 $ words s)
-    "W" -> LogMessage Warning (read $ words s!!1 :: TimeStamp) (unwords $ drop 2 $ words s)
-    "E" -> LogMessage (Error (read $ words s!!1 :: TimeStamp)) (read $ words s!!2 :: TimeStamp) 
-           (unwords $ drop 3 $ words s) 
-    _   -> Unknown s  -- Easiest to deal with 
+-- Pattern matching: use this wherever you can!
+-- Also, no need to say read 'as a timeStamp': type inferece in haskell sorts that out already 
+parseMessage s = case words s of
+    ("I":x:text)   -> LogMessage Info (read x) (unwords text)
+    ("W":x:text)   -> LogMessage Warning (read x) (unwords text)
+    ("E":x:y:text) -> LogMessage (Error (read x)) (read y) (unwords text) 
+    _              -> Unknown s  -- Easiest to deal with 
 
-parse :: String -> [LogMessage]
 -- Map function takes function and list as arguments, returns list under image of function 
+parse :: String -> [LogMessage]
 parse s = map parseMessage $ lines s
 
 ---------------------------------------------------------------------------------
@@ -32,31 +32,22 @@ parse s = map parseMessage $ lines s
 getInt :: LogMessage -> TimeStamp
 getInt (LogMessage _ a _) = a
 getInt (Unknown _) = 0
-getInt (LogMessage (Error _) b _ ) = b
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert lmX Leaf = Node Leaf lmX Leaf  -- base case, empty node/Leaf 
-insert lmX (Node t lmY s)
-    | getInt lmX <= getInt lmY = Node (insert lmX t) lmY s  -- step case, less than or equal
-    | getInt lmX > getInt lmY  = Node t lmY (insert lmX s)  -- step case, opposite to above 
+-- Unknown should be the first case in the function; otherwise tree unintentionally affected 
 insert (Unknown _) (Node t lmY s) = Node t lmY s  -- Unknowns should not affect a messageTree 
+insert lmX Leaf = Node Leaf lmX Leaf  -- base case, empty node/Leaf 
+insert lmX (Node t lmY s) = case compare (getInt lmX) (getInt lmY) of 
+    GT        -> Node t lmY (insert lmX s)
+    otherwise -> Node (insert lmX t) lmY s
 
 ---------------------------------------------------------------------------------
 -- Exercise 3: build
 ---------------------------------------------------------------------------------
 -- Makes a messageTree from a list of messages 
-
--- Getting timestamps from a list of log messages 
-getStamps :: [LogMessage] -> [TimeStamp]
-getStamps [] = []
-getStamps (lmX:lmList) = [getInt lmX] ++ getStamps lmList
-
 build :: [LogMessage] -> MessageTree
 build [] = Leaf  -- Takes empty list, produces empty node 
--- left subtree has half the list, right subtree has the other half 
-build (lmX:lmList) = Node 
-                     (build (filter (\msg -> getInt msg < getInt lmX) lmList)) lmX
-                     (build (filter (\msg -> getInt msg > getInt lmX) lmList)) 
+build (lmX:lmList) = insert lmX (build lmList)  -- Tree structure as recursive call to build 
 
 ---------------------------------------------------------------------------------
 -- Exercise 4: inOrder
@@ -82,20 +73,12 @@ sortList :: [LogMessage] -> [LogMessage]
 sortList lmList = inOrder $ build lmList
 
 -- Getting the actual description of sever error LogMessages 
-getString :: [LogMessage] -> String
-getString [(LogMessage (Error _) _ s)] = s  -- Order matters in specifying how to get strings
-getString [(Unknown _)] = ""  -- Say empty here because we are not interested in non-errors
-getString [(LogMessage _ _ _)] = "" 
-getString [] = ""
-
--- Extract messages of type error, of whose severity >= 50
-bad :: [LogMessage] -> [LogMessage]
-bad lmList = filter (\msg -> getSevere msg >= 50) lmList
+getString :: LogMessage -> String
+getString (LogMessage (Error _) _ s) = s  -- Order matters in specifying how to get strings
+getString (Unknown _) = ""  -- Say empty here because we are not interested in non-errors
+getString (LogMessage _ _ _) = "" 
 
 -- Putting the descritptions in a new list 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong [] = []
 -- Filter out all non-error messages based on their appropriate strings defined above 
-whatWentWrong (lmX:lmList) = filter (/="") (getString (bad [lmX]) : whatWentWrong (bad lmList))
-
-
+whatWentWrong messages = map getString $ filter (\msg -> getSevere msg >= 50) messages
